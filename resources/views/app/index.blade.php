246e280ac60c00aba9703e4b5d7cd36d3c171273
Med Reda Kamal <gencokm@gmail.com>
2021-12-26 01:14:32 +0100

    Leads components : add component, list component (placeholders)

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -1,4 +1,15 @@
 @extends('app.layout')
 @section('content')
-    Welcome to Leads Manager !
+    <div class="mb-3"></div>
+    <h1>Welcome to Leads Manager !</h1>
+    <p class="fst-italic text-black-50">Made by Med Reda Kamal</p>
+    <hr />
+    <div class="row">
+        <div class="col-12 col-md-3">
+            <x-leads.add />
+        </div>
+        <div class="col-12 col-md-9">
+            <x-leads.list />
+        </div>
+    </div>
 @endsection
---
+++ resources/views/components/leads/add.blade.php
@@ -0,0 +1,1 @@
+-- ADD LEAD COMPONENT --
